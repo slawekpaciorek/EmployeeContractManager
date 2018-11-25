a367dc26a451e012f34bfc2253f70cb9823349c4
slawek paciorek <slawekpaciorek@gmail.com>
2018-11-25 13:19:37 +0100

    Remove unnecessary floders

--- EmployeeContracManager/target/EmployeeContracManager-1.0-SNAPSHOT/assets/function.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    if (window.location.pathname == "/dodaj-nowa") {
-    $(".form-button").find("button:last-of-type").hide();
-}
-else {
-    $(".form-button").find("button:first-of-type").hide();
-}
-});
